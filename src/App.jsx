a87fcfb13cd6978bf454fcde52390ba545dea8fe
EmmJim <emmsa.jo@hotmail.com>
2023-03-08 13:53:26 -0600

    Login Screen, and functionality implemented

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from './components/Login'
+import Dashboard from './components/Dashboard';
+
+function App() {
+
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />}/>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    })
+
+    const [token, setToken] = useState(localStorage.getItem('token') || '');
+
+    const {email, password} = user;
+
+    useEffect(() => {
+        if(token) {
+            navigate('/dashboard')
+        }
+    }, [token])
+
+    const handleOnChange = (e) => {
+        setUser((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+
+        if(email === '' || password === ''){
+            return console.log('Error');
+        }
+
+
+        console.log(JSON.stringify(user))
+
+        const result = await fetch('http://localhost:4000/api/auth/login', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+        const data = await result.json();
+
+        if(data.token){
+            localStorage.setItem('token', data.token);
+            setToken(data.token)
+        }
+
+    }
+
+    return (
+        <div className='h-screen flex flex-col justify-center items-center'>
+            <h1 className='text-6xl text-white font-bold mb-8 titulo'>Do it!</h1>
+            <div className='bg-white w-1/3 rounded-lg p-6 space-y-8 shadow-md shadow-black'>
+                <div className='space-y-4'>
+                    <h2 className='font-bold text-2xl text-center'>Sign In</h2>
+                    <hr />
+                </div>
+                <form className='mt-6 space-y-5' onSubmit={handleOnSubmit}>
+                    <div className='flex flex-col space-y-3'>
+                        <label className='font-bold'>Email:</label>
+                        <input className='border border-indigo-400 rounded-lg p-1' type="text" placeholder='Your email here' name="email" value={email} onChange={handleOnChange} />
+                    </div>
+                    <div className='flex flex-col space-y-3'>
+                        <label className='font-bold'>Password:</label>
+                        <input className='border border-indigo-400 rounded-lg p-1' type="text" placeholder='Your password here' name="password" value={password} onChange={handleOnChange}/>
+                    </div>
+                    <div className='flex justify-center'>
+                        <input className='bg-indigo-700 text-white rounded-lg p-2 font-bold hover:bg-indigo-900 hover:scale-125 ease-out duration-300 hover:cursor-pointer' type="submit" value="Log In" />
+                    </div>
+                </form>
+                <div className='flex justify-end'>
+                    <p>Don't have an account yet? <span className='font-bold text-indigo-700 hover:cursor-pointer'>Sign Up Here!</span></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
